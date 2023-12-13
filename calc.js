2c4be2fcbeb6680857c4cfe2c9a6c2209b847601
Jack Sarles <jack.sarles@mcmcg.com>
2023-12-13 14:30:11 -0800

    Add documentation

--- calc.js
+++ calc.js
@@ -1,20 +1,44 @@
-// add function 
+/**
+ * Adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
 function add(a, b) {
     return a + b;
 }
-// subtract function
+
+/**
+ * Subtracts two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The difference between the two numbers.
+ */
 function subtract(a, b) {
     return a - b;
 }
-// multiply function
+
+/**
+ * Multiplies two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The product of the two numbers.
+ */
 function multiply(a, b) {
     return a * b;
 }
-// divide function
+
+/**
+ * Divides two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The quotient of the two numbers.
+ */
 function divide(a, b) {
     return a / b;
 }
-//  Create a variable for the first number
+
+// Create a variable for the first number
 let a = 0;
 
 // Create a variable for the operator. 
@@ -23,6 +47,6 @@ let operator = '';
 // Create a variable for the second number.
 let b = 0;
 
-// Create and update a display varible when the number buttons are clicked
+// Create and update a display variable when the number buttons are clicked
 let display = document.getElementById('display');
 let displayValue = '';
