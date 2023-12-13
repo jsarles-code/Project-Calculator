842b2c6097795f1940ed83eca88f8bdebf891655
Jack Sarles <jack.sarles@mcmcg.com>
2023-12-13 14:14:43 -0800

    Fix script tag in calc.html

--- calc.js
+++ calc.js
@@ -23,10 +23,6 @@ let operator = '';
 // Create a variable for the second number.
 let b = 0;
 
-// Create an event listener that listens for the number buttons 0-9 and displays them in the result field when clicked.
-const numbers = document.querySelectorAll('.number');
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-        document.querySelector('#result').value += number.value;
-    });
-});
+// Create and update a display varible when the number buttons are clicked
+let display = document.getElementById('display');
+let displayValue = '';
