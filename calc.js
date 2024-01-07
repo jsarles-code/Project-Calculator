a126488fbd960648739d0e8d0ed99a5060dc522b
jsarles-code <57759984+jsarles-code@users.noreply.github.com>
2024-01-07 13:51:36 -0800

    trying again

--- calc.js
+++ calc.js
@@ -1,52 +1,25 @@
-// Create variables to store the values 
-let firstNumber;
-let operator;
-let secondNumber;
-
-// Create a function to operate on the numbers
-function operate(operator, num1, num2) {
-  if (operator === '+') {
-    return num1 + num2;
-  } else if (operator === '-') {
-    return num1 - num2;
-  } else if (operator === '*') {
-    return num1 * num2;
-  } else if (operator === '/') {
-    return num1 / num2; 
-  }
-}
-
-// Function to clear the values
-function clear() {
-  firstNumber = '';
-  operator = '';
-  secondNumber = '';
-}
-
-// Function to display values
-function display(val) {
-  document.getElementById('display').innerText = val; 
-}
-
-// On click of number buttons
-function handleNumberClick(num) {
-  if (!operator) {
-    firstNumber += num;
-    display(firstNumber); 
-  } else {
-    secondNumber += num;
-    display(secondNumber);
-  }
-}
-
-// On click of operator buttons
-function handleOperatorClick(op) {
-  operator = op;
-} 
-
-// On click of equal button
-function handleEqualClick() {
-  let result = operate(operator, firstNumber, secondNumber);
-  display(result);
-  clear();
-}
+function appendToDisplay(value) {
+      document.getElementById('display').value += value;
+    }
+    
+    function clearDisplay() {
+      document.getElementById('display').value = '';
+    }
+    
+    function evaluate() {
+      let expression = document.getElementById('display').value;
+      let result = calculateExpression(expression);
+      if (result === null) {
+        document.getElementById('display').value = 'Error';
+      } else {
+        document.getElementById('display').value = result;
+      }
+    }
+    
+    function calculateExpression(expression) {
+      try {
+        return Function('"use strict";return (' + expression + ')')();
+      } catch (error) {
+        return null;
+      }
+    }
