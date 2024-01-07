dd154b083a0fe051b935d28efe0ab5d1d92215b0
jsarles-code <57759984+jsarles-code@users.noreply.github.com>
2024-01-07 14:03:07 -0800

    commit

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { document } from 'some-library';
 import { getElementById } from 'some-library';
 function appendToDisplay(value) {
@@ -25,3 +26,57 @@ function appendToDisplay(value) {
         return null;
       }
     }
+=======
+// Create variables to store the values 
+let firstNumber;
+let operator;
+let secondNumber;
+
+// Create a function to operate on the numbers
+function operate(operator, num1, num2) {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    return num1 / num2; 
+  }
+}
+
+// Function to clear the values
+function clear() {
+  firstNumber = '';
+  operator = '';
+  secondNumber = '';
+}
+
+// Function to display values
+function display(val) {
+  document.getElementById('display').innerText = val; 
+}
+
+// On click of number buttons
+function handleNumberClick(num) {
+  if (!operator) {
+    firstNumber += num;
+    display(firstNumber); 
+  } else {
+    secondNumber += num;
+    display(secondNumber);
+  }
+}
+
+// On click of operator buttons
+function handleOperatorClick(op) {
+  operator = op;
+} 
+
+// On click of equal button
+function handleEqualClick() {
+  let result = operate(operator, firstNumber, secondNumber);
+  display(result);
+  clear();
+}
+>>>>>>> parent of a126488 (trying again)
